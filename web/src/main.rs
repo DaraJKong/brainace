29cbeedfdcdf97a7507c75edd980c05836d399a9
DaraJKong <daraetkong@gmail.com>
2024-01-20 01:18:48 -0500

    Use TailwindCSS

--- web/src/main.rs
+++ web/src/main.rs
@@ -10,7 +10,7 @@ fn app(cx: Scope) -> Element {
     let mut count = use_state(cx, || 0);
 
     cx.render(rsx! {
-        h1 { "High-Five counter: {count}" }
+        h1 { class: "text-emerald-500", "High-Five counter: {count}" }
         button { onclick: move |_| count += 1, "Up high!" }
         button { onclick: move |_| count -= 1, "Down low!" }
     })
