90a097a3111f5dbd831bd6383c1b26022c002fe5
DaraJKong <daraetkong@gmail.com>
2024-01-27 23:34:38 -0500

    Migrate web to leptos instead of dioxus

---
+++ web/src/lib.rs
@@ -0,0 +1,18 @@
+pub mod app;
+pub mod auth;
+pub mod error_template;
+pub mod errors;
+#[cfg(feature = "ssr")]
+pub mod fallback;
+#[cfg(feature = "ssr")]
+pub mod state;
+
+#[cfg(feature = "hydrate")]
+#[wasm_bindgen::prelude::wasm_bindgen]
+pub fn hydrate() {
+    use crate::app::*;
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+
+    leptos::mount_to_body(App);
+}
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,11 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    mode: "all",
-    content: [
-        // include all rust, html and css files in the src directory
-        "./src/**/*.{rs,html,css}",
-        // include all html files in the output (dist) directory
-        "./dist/**/*.html",
-    ],
+    content: ["*.html", "./src/**/*.rs"],
     theme: {
         extend: {},
     },
